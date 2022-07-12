6de7444ff452c54efbeefbc311998577e36ee23c
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-07-12 13:15:44 -0600

    add modules and refactor awesome books

---
+++ addBooks.js
@@ -0,0 +1,27 @@
+import Book from './modules/books.js';
+import UI from './modules/uiClass.js'
+// add book event
+export default () =>{
+document.querySelector('#form-Book').addEventListener('submit',
+(e) => {
+  // prevent actual submit
+  e.preventDefault();
+  // get form values
+  const author = document.getElementById('authorname').value;
+  const title = document.getElementById('titlename').value;
+  // instatiate book
+   const book = new Book(author, title);
+
+  // adding book to ui
+  UI.addBookToList(book);
+  // adding book to store
+   UI.addBook(book);
+});
+
+// remove event
+document.querySelector('#tablebody').addEventListener('click', (e) => {
+UI.deleteBook(e.target);
+UI.removeBook(e.target);
+});
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+import UI from "./modules/uiClass.js";
+import addBooks from "./addBooks.js";
+
+addBooks();
+
+  const date = document.querySelector('.date');
+  date.innerHTML = Date();
+  
+  document.addEventListener('DOMContentLoaded', UI.displayBooks);
+  // adding navigation buttons
+  const list = document.querySelector('#list-btn');
+  const addNew = document.querySelector('#add-btn');
+  const contacts = document.querySelector('#contact-btn');
+  const addBook = document.querySelector('#new-book');
+  const contactForm = document.querySelector('#contact');
+  const bookSections = document.querySelector('#all-books');
+  
+  // adding events
+  
+  list.addEventListener('click', () => {
+    addBook.classList.add('inactive');
+    contactForm.classList.add('inactive');
+    bookSections.classList.remove('inactive');
+  });
+  
+  addNew.addEventListener('click', () => {
+    addBook.classList.remove('inactive');
+    bookSections.classList.add('inactive');
+    contactForm.classList.add('inactive');
+  });
+  
+  contacts.addEventListener('click', () => {
+    contactForm.classList.remove('inactive');
+    bookSections.classList.add('inactive');
+    addBook.classList.add('inactive');
+  });
+
